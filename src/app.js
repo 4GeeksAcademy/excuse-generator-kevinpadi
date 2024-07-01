02b8cb76e437f10d9ccae9b99cdbe52c8482ae97
Kevin Padilla <kevpadilla01@gmail.com>
2024-07-01 18:16:04 +0000

    projecto terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const excuse = document.getElementById("excuse");
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateExcuse();
 };
+
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
+function generateRandomNumber(arr) {
+  return Math.floor(Math.random() * arr.length);
+}
+
+function generateExcuse() {
+  excuse.innerHTML = `${who[generateRandomNumber(who)]} ${
+    action[generateRandomNumber(action)]
+  } ${what[generateRandomNumber(what)]} ${when[generateRandomNumber(when)]}`;
+}
