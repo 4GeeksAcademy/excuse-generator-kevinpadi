494b9f0c64f6dbe66625eaae0b686424fd40a2eb
Kevin Padilla <kevpadilla01@gmail.com>
2024-07-04 11:39:52 -0300

    codigo refactorizado

--- src/app.js
+++ src/app.js
@@ -5,8 +5,6 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-const excuse = document.getElementById("excuse");
-
 window.onload = function() {
   generateExcuse();
 };
@@ -14,20 +12,13 @@ window.onload = function() {
 let who = ["The dog", "My grandma", "The mailman", "My bird"];
 let action = ["ate", "peed", "crushed", "broke"];
 let what = ["my homework", "my phone", "the car"];
-let when = [
-  "before the class",
-  "when I was sleeping",
-  "while I was exercising",
-  "during my lunch",
-  "while I was praying"
-];
+let when = ["before the class", "when I was sleeping", "while I was exercising", "during my lunch", "while I was praying"];
 
 function generateRandomNumber(arr) {
   return Math.floor(Math.random() * arr.length);
 }
 
 function generateExcuse() {
-  excuse.innerHTML = `${who[generateRandomNumber(who)]} ${
-    action[generateRandomNumber(action)]
-  } ${what[generateRandomNumber(what)]} ${when[generateRandomNumber(when)]}`;
+  let excuse = document.getElementById("excuse");
+  excuse.innerHTML = `${who[generateRandomNumber(who)]} ${action[generateRandomNumber(action)]} ${what[generateRandomNumber(what)]} ${when[generateRandomNumber(when)]}`;
 }
